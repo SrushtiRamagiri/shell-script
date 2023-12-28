35e79e48a6bebfa9b0f2ad67b4c761095e8d8df6
Srushti <ramagirisrushti@gmail.com>
2023-12-28 23:35:55 +0530

    new

--- 05-read.sh
+++ 05-read.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 echo "pls enter first person name :"
-
 read NAME
 
 echo "pls enter second person name :"
 read -s NAME1
 
+read -p "Enter Your Name:" myName
+
+
 echo "$NAME"
 
 echo "$NAME1"
+
+echo "$myName"
