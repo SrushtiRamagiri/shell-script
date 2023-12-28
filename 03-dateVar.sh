a921924da8e4e7de9eb05b67d3be8d51306cac62
Srushti <ramagirisrushti@gmail.com>
2023-12-28 23:31:55 +0530

    new

--- 03-dateVar.sh
+++ 03-dateVar.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 DATE=$(date)
+list=$(ls -l)
+listall=$(ls -a)
+exitstatus=$(sudo useradd saiteja)
 
 echo "Date is : ${DATE}"
 
 echo "Date is : $DATE"
+
+echo "listind directory: $list"
+
+echo "listind all files including hidden directory: $listall"
+
+
 
