87ea29cadc8427d139b47929a350c1d201ebca54
Srushti <ramagirisrushti@gmail.com>
2023-12-30 22:53:47 +0530

    Conditions

--- 06-Arrays.sh
+++ 06-Arrays.sh
@@ -5,3 +5,4 @@ FRUITS=["APPLE","PEAR","BANANA"]
 echo "first value: ${FRUITS[0]}"
 echo "second value: ${FRUITS[1]}"
 echo "Third value : ${FRUITS[2]}"
+echo "Third value : ${FRUITS[@]}"
---
+++ 07-condition.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+NUM=$1
+
+if[ $NUM -gt 100 ]
+then
+    echo "$NUM is greater than 100"
+else 
+    echo "$NUM is not greater than 100"
+fi
