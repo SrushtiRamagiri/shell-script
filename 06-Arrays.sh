36592ec792ffdad4e74082009ab0259aa6f12880
Srushti <ramagirisrushti@gmail.com>
2023-12-31 18:16:26 +0530

    conditions

--- 06-Arrays.sh
+++ 06-Arrays.sh
@@ -5,4 +5,4 @@ FRUITS=("APPLE" "PEAR" "BANANA")
 echo "first value: ${FRUITS[0]}"
 echo "second value: ${FRUITS[1]}"
 echo "Third value : ${FRUITS[2]}"
-echo "Third value : ${FRUITS[@]}"
+echo "All  Array value : ${FRUITS[@]}"
